acfb85ce21c7b3798ac417dc36b98c45f31625ee
Chris Roberts <therstyle@gmail.com>
2019-10-11 20:53:14 -0400

    Restructure directories and functions

--- searchform.php
+++ searchform.php
@@ -5,9 +5,10 @@
  * @package rstyle
  */
 ?>
+
 <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
 	<label>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'rstyle' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex('Search for:', 'label', 'rstyle'); ?>">
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'rstyle'); ?>" value="<?php echo esc_attr(get_search_query()); ?>" name="s" title="<?php _ex('Search for:', 'label', 'rstyle'); ?>">
 	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button', 'rstyle'); ?>">
 </form>
