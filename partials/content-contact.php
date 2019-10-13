7694559985ddd37445ab486de5ab4d9c6bb90400
therstyle <therstyle@gmail.com>
2019-10-13 14:11:17 -0400

    Updated footer vars

--- partials/content-contact.php
+++ partials/content-contact.php
@@ -5,6 +5,8 @@ require_once get_template_directory().'/Controllers/Footer.php';
 use Controllers\Footer as Footer;
 $footer = new Footer();
 
+extract(get_vars($footer))
+
 ?>
 
 <section id="quick-contact">
@@ -13,20 +15,20 @@ $footer = new Footer();
       <div class="col-xs-12">
         <div class="row">
           <div class="col-md-4 grid-space">
-            <?php if($footer->get_contact()['headline'] || $footer->get_contact()['text']): ?>
-              <?php if($footer->get_contact()['headline']): ?>
-                <h2 class="headline"><?php echo $footer->get_contact()['headline']; ?></h2>
+            <?php if($get_contact['headline'] || $get_contact['text']): ?>
+              <?php if($get_contact['headline']): ?>
+                <h2 class="headline"><?php echo $get_contact['headline']; ?></h2>
               <?php endif; ?>
 
-              <?php if($footer->get_contact()['text']): ?>
-                <?php echo $footer->get_contact()['text']; ?>
+              <?php if($get_contact['text']): ?>
+                <?php echo $get_contact['text']; ?>
               <?php endif; ?>
             <?php endif; ?>
             
-            <?php if($footer::get_social()): ?>
+            <?php if($get_social): ?>
   						<div class="lg-top-space">
                 <?php 
-                foreach($footer::get_social()['social'] as $social) {
+                foreach($get_social['social'] as $social) {
                   set_query_var('url', $social['url']);
                   set_query_var('type', $social['type']);
                   get_template_part('partials/content','social');
@@ -37,7 +39,7 @@ $footer = new Footer();
           </div>
 
           <div class="col-md-7 col-md-offset-1">
-            <?php echo do_shortcode('[formidable id='.$footer->get_contact()['id'].']'); ?>
+            <?php echo do_shortcode('[formidable id='.$get_contact['id'].']'); ?>
           </div>
         </div>
       </div>
