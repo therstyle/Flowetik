d8fd4530271148c710d12c2a3a031ff518e676e8
therstyle <therstyle@gmail.com>
2019-10-13 10:55:03 -0400

    Registered post types

--- functions.php
+++ functions.php
@@ -46,6 +46,79 @@ function rstyle_widgets_init() {
 
 add_action('widgets_init', 'rstyle_widgets_init');
 
+function register_cpts() {
+	/**
+	 * Post Type: Team Members.
+	 */
+
+	$labels = array(
+		"name" => __( "Team Members", "rstyle" ),
+		"singular_name" => __( "Team Member", "rstyle" ),
+	);
+
+	$args = array(
+		"label" => __( "Team Members", "rstyle" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => false,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "bio", "with_front" => true ),
+		"query_var" => true,
+		"supports" => array( "title", "editor", "thumbnail" ),
+	);
+
+	register_post_type( "bio", $args );
+
+	/**
+	 * Post Type: Testimonials.
+	 */
+
+	$labels = array(
+		"name" => __( "Testimonials", "rstyle" ),
+		"singular_name" => __( "Testimonial", "rstyle" ),
+	);
+
+	$args = array(
+		"label" => __( "Testimonials", "rstyle" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => false,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "testimonial", "with_front" => true ),
+		"query_var" => true,
+		"supports" => array( "title", "editor" ),
+	);
+
+	register_post_type( "testimonial", $args );
+}
+
+add_action( 'init', 'register_cpts' );
+
+
 // Enqueue scripts and styles
 function rstyle_scripts() {
 	if (!is_admin()) {
